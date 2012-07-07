d96e939f7c46e34871772c64e2123d33a2b7b4fc
Ashwini Khare <khare.ashwni@gmail.com>
2012-07-08 03:43:15 +0530

    Merged script.js of input

--- input/js/script.js
+++ input/js/script.js
@@ -40,13 +40,9 @@ $(document).ready(function(){
 				]
 			});
 			
-<<<<<<< HEAD
-			$('#input-table').after("<input class='btn' type='button' value='Done'><input calss='btn' type='button' value='Reset'>");
-			$("input[value='Done!']").on('click',function(){
-=======
 			$('#input-table').after("<br/><br/><input class='btn' type='button' value='Done'><input class='btn' type='button' value='Reset'><input class='btn' type='button' value='Show/Hide'>");
 			$("input[value='Done']").on('click',function(){
->>>>>>> refactoring code
+
 			console.log($("#input-table").inputtable('getData'));
 				$('#input-table').after('Please check your console for the array');
 				return false;
@@ -55,14 +51,6 @@ $(document).ready(function(){
 				$("#input-table").inputtable('clear');
 				return false;
 			});
-<<<<<<< HEAD
-=======
-			$("input[value='Show/Hide']").on('click',function(){
-				alert('working on it');
-				
-				return false;
-			});
->>>>>>> refactoring code
 			
 		
 		
